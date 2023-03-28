311af11900fef44d7eb611a76c57adabae9157b3
MinhDuy <98084393+MinhDuy18@users.noreply.github.com>
2023-03-28 10:31:27 +0700

    ChanhAnC

--- mavenproject1/src/main/java/GUI/QL_CongDoan_GUI.java
+++ mavenproject1/src/main/java/GUI/QL_CongDoan_GUI.java
@@ -2,17 +2,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
+package GUI;
 
 /**
  *
  * @author duy19
  */
-public class TrangChu_GUI1 extends javax.swing.JFrame {
+public class QL_CongDoan_GUI extends javax.swing.JFrame {
 
     /**
-     * Creates new form TrangChu_GUI1
+     * Creates new form QL_CongDoan_GUI
      */
-    public TrangChu_GUI1() {
+    public QL_CongDoan_GUI() {
         initComponents();
     }
 
@@ -58,20 +59,20 @@ public class TrangChu_GUI1 extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TrangChu_GUI1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(QL_CongDoan_GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TrangChu_GUI1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(QL_CongDoan_GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TrangChu_GUI1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(QL_CongDoan_GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TrangChu_GUI1.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(QL_CongDoan_GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new TrangChu_GUI1().setVisible(true);
+                new QL_CongDoan_GUI().setVisible(true);
             }
         });
     }
